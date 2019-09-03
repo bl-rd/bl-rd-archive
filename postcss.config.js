b8711cdc40369c7ea1703f9341f434b9edffab87
Bill Reid <william.c.reid@hotmail.co.uk>
2019-09-03 12:52:19 +0100

    Small style tweaks, add more postcss plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,11 @@
 const postcssPresetEnv = require('postcss-preset-env');
+const postcssImport = require('postcss-import');
+const cssnano = require('cssnano');
 
 module.exports = {
   plugins: [
-    postcssPresetEnv()
+    postcssImport(),
+    postcssPresetEnv(),
+    cssnano()   
   ]
 };
