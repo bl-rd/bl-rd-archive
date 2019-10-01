aaf768d2dfce2599d2d24d1733b28420513fd1c6
Bill <me@bl-rd.dev>
2019-10-01 12:43:25 +0100

    Initial commit of menu button custom element

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,14 @@
-const fs = require("fs");
+const fs = require('fs');
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.setBrowserSyncConfig({
     callbacks: {
-      ready: function(err, bs) {
-        const content_404 = fs.readFileSync('_site/404/index.html');
+      ready: async function(err, bs) {
+        const notFoundTemplate = fs.readFileSync('_site/404/index.html');
 
         bs.addMiddleware("*", (req, res) => {
           // Provides the 404 content without redirect.
-          res.write(content_404);
+          res.write(notFoundTemplate);
           res.end();
         });
       }
