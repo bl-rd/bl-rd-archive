05955857325432eb6031756939b7e8aed825b671
Bill <me@bl-rd.dev>
2019-11-26 21:23:49 +0000

    Update the landing title based on the time of day

---
+++ themes/test/assets/js/landing.js
@@ -0,0 +1,21 @@
+/**
+ * Update the landing message based on the user's
+ * time of day.
+ */
+(function() {
+    const element = document.querySelector('h1');
+    
+    const hour = (new Date()).getHours();
+
+    let msg = 'Good day';
+
+    if (hour < 12) {
+        msg = 'Good morning';
+    } else if (hour < 18) {
+        msg = 'Good afternoon';
+    } else {
+        msg = 'Good evening';
+    }
+
+    element.innerText = msg;
+})();
